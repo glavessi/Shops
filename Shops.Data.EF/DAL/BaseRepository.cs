0643dca62a9496de02b90d96ad7485132f7ea2e2
gsosa <glavessi6978@gmail.com>
2022-08-02 00:34:59 -0600

    Added base class and interface for datalayer

---
+++ Shops.Data.EF/DAL/BaseRepository.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shops.Data.EF.Data;
+using Shops.Data.EF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shops.Data.EF.DAL
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        private bool disposedValue;
+        protected readonly ChallengeDBContext _Context;
+
+        public BaseRepository(ChallengeDBContext context)
+        {
+            _Context = context;
+        }
+
+        public async Task<int> CommitChanges()
+        {
+            return await _Context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _Context.Set<TEntity>().FindAsync(id);
+            if (entity == null)
+            {
+                throw new Exception($"{nameof(id)} could not be found.");
+            }
+
+            try
+            {
+                _Context.Set<TEntity>().Remove(entity);
+                int deleted = await CommitChanges();
+
+                if (deleted > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't delete the registry: {ex.Message}");
+            }
+        }
+
+        public async Task<IList<TEntity>> GetAllAsync()
+        {
+            try
+            {
+                return await _Context.Set<TEntity>().AsNoTracking().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retrieve registries: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> GetByIdAsync(int[] id)
+        {
+            try
+            {
+                return await _Context.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't find registry with id={id.ToString()}: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> InsertAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            try
+            {
+                await _Context.Set<TEntity>().AddAsync(entity);
+                int saved = await CommitChanges();
+
+                if (saved > 0)
+                    return entity;
+                else 
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be saved: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            try
+            {
+                _Context.Set<TEntity>().Update(entity);
+                int updated = await CommitChanges();
+
+                if (updated > 0)
+                    return entity;
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be updated: {ex.Message}");
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~BaseRepository()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Shops.Data.EF/DAL/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shops.Data.EF.DAL
+{
+
+    //Base repository to be used across all entities repos
+    public interface IBaseRepository<TEntity> : IDisposable
+    {
+        Task<IList<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int[] id);
+        Task<TEntity> InsertAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(int id);
+        Task<int> CommitChanges();
+    }
+}
